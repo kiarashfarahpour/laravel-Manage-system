5bae33b062f52a481541bb57ab6186d7b14dafb2
darklord@12321 <kiarashfarahpour@gmail.com>
2024-06-12 11:00:21 +0200

    Upload laravel Prpject

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>By &copy; <?php echo date('Y'); ?> </strong>
+    Kiarash Farahpour.
+    <div class="float-right d-none d-sm-inline-block">
+
+    </div>
+</footer>
